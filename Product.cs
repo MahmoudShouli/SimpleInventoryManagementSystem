e1f76f389976e6cc0567e67d80998e0fb5baac2c
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-02-26 18:45:35 +0200

    Create Product class

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleInventoryManagementSystem;
+
+public class Product
+{
+    private string name = string.Empty;
+    private float price;
+    private int quantity;
+    
+    public string Name { get => name; set => name = value; }
+    public float Price { get => price; set => price = value; }
+    public int Quantity { get => quantity; set => quantity = value; }
+
+    public Product(string name, float price, int quantity)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Quantity = quantity;
+    }
+}
