62b4c9a66bf636c73ae00aa097444498ecf76dfd
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-14 23:42:42 +0300

    Add Id field.

--- Product.cs
+++ Product.cs
@@ -2,24 +2,21 @@
 
 public class Product
 {
+    public int Id { get; set; }
     public string Name { get; private set; } = "";
     private double Price { get; set; }
     private int Quantity { get; set; }
 
-    public Product(string name, double price, int quantity)
+    public Product(int id, string name, double price, int quantity)
     {
-        EditProduct(name, price, quantity);
+        this.Id = id;
+        this.Name = name;
+        this.Price = price;
+        this.Quantity = quantity;
     }
     
     public override string ToString()
     {
         return $"Name: {this.Name}, Price: ${this.Price}, Quantity: {this.Quantity}";
-    }
-
-    public void EditProduct(string name, double price, int quantity)
-    {
-        this.Name = name;
-        this.Price = price;
-        this.Quantity = quantity;
     }
 }
