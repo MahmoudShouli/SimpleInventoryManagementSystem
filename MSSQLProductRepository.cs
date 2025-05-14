249ed88010d7d2a8cd6ae6aa87e215c6bc71066c
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-15 00:35:44 +0300

    MS SQL implementation of Product repo

---
+++ MSSQLProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SimpleInventoryManagementSystem;
+
+public class MssqlProductRepository(SqlConnection conn) : IProductRepository
+{
+    public void AddProduct(Product product)
+    {
+        using (conn)
+        {
+            conn.Open();
+            var query = "INSERT INTO Products (Name, Price, Quantity) VALUES (@name, @price, @quantity)";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@name", product.Name);
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@quantity", product.Quantity);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        var products = new List<Product>();
+        
+        using (conn)
+        {
+            conn.Open();
+            
+            var query = "SELECT * FROM Products";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);        
+                    string name = reader.GetString(1);  
+                    decimal price = reader.GetDecimal(2); 
+                    int quantity = reader.GetInt32(3);  
+
+                    products.Add(new Product(id, name, price, quantity));
+                }
+            }
+        }
+        
+        return products;
+    }
+
+    public void DeleteProduct(int id)
+    {
+        using (conn)
+        {
+            conn.Open();
+            var query = "DELETE FROM Products WHERE Id = @id";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        using (conn)
+        {
+            conn.Open();
+            var query = "UPDATE Products SET Name = @name, Price = @price, Quantity = @quantity WHERE Id = @id";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@name", product.Name);
+                cmd.Parameters.AddWithValue("@price", product.Price);
+                cmd.Parameters.AddWithValue("@quantity", product.Quantity);
+                cmd.Parameters.AddWithValue("@id", product.Id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
