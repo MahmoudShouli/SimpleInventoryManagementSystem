992bc7ab22285f090c2aff182ae4f446bc88c3a8
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-14 23:56:21 +0300

    Change access modifiers

--- Product.cs
+++ Product.cs
@@ -3,9 +3,9 @@
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; private set; } = "";
-    private double Price { get; set; }
-    private int Quantity { get; set; }
+    public string Name { get; set; } 
+    public double Price { get; set; }
+    public int Quantity { get; set; }
 
     public Product(int id, string name, double price, int quantity)
     {
