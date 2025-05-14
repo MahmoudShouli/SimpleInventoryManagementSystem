955c3b8455424dee4598d038e7dcf84f677fc0e6
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-15 00:35:23 +0300

    Abstraction for Product repo

---
+++ IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleInventoryManagementSystem;
+
+public interface IProductRepository
+{
+    void AddProduct(Product product);
+    List<Product> GetAllProducts();
+    void DeleteProduct(int id);
+    void UpdateProduct(Product product);
+}
